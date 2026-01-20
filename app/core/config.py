3ae819126b7ec82affd6a5560eb3d57c211ce5b8
Andrey Sapeshko <andrdd17@gmail.com>
2026-01-20 15:18:45 +0300

    Изменения в config

--- app/core/config.py
+++ app/core/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     POSTGRES_USER: str = "my_user"
     POSTGRES_PASSWORD: str = "my_password"
     POSTGRES_HOST: str = "localhost"
-    POSTGRES_PORT: str = 5432
+    POSTGRES_PORT: str = "5432"
     CELERY_BROKER_URL: str = "redis://redis:6379/0"
     CELERY_RESULT_BACKEND: str = "redis://redis:6379/1"
     DERIBIT_BASE_URL: str = "https://www.deribit.com/api/v2"
